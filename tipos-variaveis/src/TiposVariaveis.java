766d0097c47fcb3ccf965c0903aa8484af66acaf
Nayane-Jacyara <njcs@discente.ifpe.edu.br>
2023-01-26 15:12:03 -0300

    Tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,26 @@
 public class TiposVariaveis {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) {
+        // Tipos de variaveis 
+        //estudem a class String que representa texto de aplicacao 
+
+        String meuNome = "Nayane Jacyara";
+
+        byte idade = 123;
+		short ano = 2021;
+		int cep = 21070333; // se começar com zero, talvez tenha que ser outro tipo
+		long cpf = 98765432109L; // se começar com zero, talvez tenha que ser outro tipo
+		float pi = 3.14F;
+		double salario = 1275.33;
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        final int NUMERO = 5;
+
+        NUMERO = 10;// colocando o final no comeco da variavel nao podera altera o valor sempre vai ser fixo 
+
+        System.out.println(NUMERO);
+}
 }
