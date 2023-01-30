81a3df1a38e29914636accd876c61f21bb4cabe0
Nayane-Jacyara <njcs@discente.ifpe.edu.br>
2023-01-30 18:00:08 -0300

    Aula de  operadores 5

--- tipos-variaveis/src/TiposDeOperadores.java
+++ tipos-variaveis/src/TiposDeOperadores.java
@@ -1,6 +1,7 @@
 import java.util.Date;
 
 public class TiposDeOperadores{
+   
     public static void main(String[] args) throws Exception {
 
     //O operador de atribuição é utilizado para definir o valor inicial ou sobrescrever o valor de uma variável.
@@ -71,5 +72,29 @@ public class TiposDeOperadores{
 
     System.out.println("Inverteu " + !verdadeiro);
 
-    }
+    /*Operador Ternario 
+    O operador de condição ternária é uma forma resumida para definir uma condição e escolher por um dentre dois valores. Você deve pensar numa condição ternária, como se fosse uma condição IF normal, porém, de uma forma em que toda a sua estrutura estará escrita numa única linha.
+    O operador ternário é representado pelos símbolos "?" e ":" utilizados na seguinte estrutura de sintaxe:
+    <Expressão Condicional>`` ?``<Caso condição seja true>``: ``<Caso condição seja false>*/
+
+    int a, b;
+
+    a = 5;
+    b = 6;
+
+    /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+    if(a==b)
+       resultado = "verdadeiro";
+       else
+        resultado = "falso";
+    */
+
+    //MESMA CONDICIONAL, MAS DESSA VEZ, UTILIZANDO O OPERADOR CONDICIONAL TERNÁRIO
+    String resultado = (a==b) ? "verdadeiro" : "false";
+
+    System.out.println(resultado);  
+
+
+    /* */
+   }
 }
