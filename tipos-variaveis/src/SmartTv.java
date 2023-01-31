75798d0a2a864c9737eaad2a5036fdfc4c85072f
Nayane-Jacyara <njcs@discente.ifpe.edu.br>
2023-01-30 23:32:04 -0300

    Aula de metodos , exercitando com smartTV

--- tipos-variaveis/src/SmartTv.java
+++ tipos-variaveis/src/SmartTv.java
@@ -2,7 +2,7 @@ public class SmartTv {
 
     boolean ligada = false;
     int canal = 1;
-    int volume = 25; 
+    int volume = 24; 
 
     public void mudarCanal(int novoCanal){
         canal = novoCanal;
